d0d416d9a3ff227ad97ba0f4fac245572e03b9a1
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2024-07-26 00:33:16 -0300

    Created Project API Java

---
+++ Car-Wash-Backend/src/test/java/me/jcs/Car/Wash/CarWashApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jcs.Car.Wash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
